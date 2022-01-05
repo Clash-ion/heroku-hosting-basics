935e370372532e2b280b52e792ed1f61fda52c69
Mukul Kumar <mukul0000kumar@gmail.com>
2022-01-05 12:32:47 +0530

    a1

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 # from app.main import app
 from flask import Flask
-
+# def helpful_function():
 app = Flask(__name__)
-
+# application = app
 
 @app.route("/")
 def fun():
     return "<h1>hello world</h1>"
 
+    # return app
+
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app)
-    # app.run()
+    # from waitress import serve
+    # serve(app)
+    app.run()
